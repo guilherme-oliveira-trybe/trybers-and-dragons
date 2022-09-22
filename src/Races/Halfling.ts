ba8fbee8167713d41d0796fffad17a8047f607d5
Guilherme Oliveira <guilherme.moliveira@hotmail.com>
2022-09-22 15:54:09 -0300

    feat: Implementa class Halfling extends Race

---
+++ src/Races/Halfling.ts
@@ -0,0 +1,22 @@
+import Race from './Race';
+
+class Halfling extends Race {
+  private static _racesInstances = 0;
+  private _maxLifePoints: number;
+
+  constructor(name: string, dexterity: number) {
+    super(name, dexterity);
+    this._maxLifePoints = 60;
+    Halfling._racesInstances += 1;
+  }
+
+  get maxLifePoints(): number {
+    return this._maxLifePoints;
+  }
+
+  static createdRacesInstances(): number {
+    return this._racesInstances;
+  }
+}
+
+export default Halfling;
